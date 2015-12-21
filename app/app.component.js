c7d6ed5da94298d517934f0b8aea8a3f93522b00
AlbertoFuente <alberto.delafuente.adesis@gmail.com>
2015-12-21 22:10:27 +0100

    Playing with Angular2 Beta

--- app/app.component.js
+++ app/app.component.js
@@ -3,6 +3,9 @@
         .Component({
             selector: 'my-app',
             template: `
+            <h2>Change your Name</h2>
+            <input #cmn>
+            <button (click)="changeMyName(cmn.value)">Change Your Name</button>
             <h2>My name is {{ myName }}</h2>
             <p>Names list: </p>
             <ul>
@@ -10,12 +13,22 @@
                 {{ name }}
                 </li>
             </ul>
+            <h2>Add more names: </h2>
+            <input #addName>
+            <button (click)="addNames(addName.value)">Add Name</button>
             `
         })
         .Class({
             constructor: function() {
                 this.myName = 'Alberto';
                 this.names = ['Pedro', 'Juan', 'María', 'Mario', 'Carlos'];
+            },
+            addNames: function(n) {
+                this.names.push(n);
+            },
+            changeMyName: function(n) {
+                this.myName = n;
             }
+
         });
 })(window.app || (window.app = {}));
