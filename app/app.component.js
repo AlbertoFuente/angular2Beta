37e5cb1437b2da8827965fb3eb2bc2ad672631be
AlbertoFuente <alberto.delafuente.adesis@gmail.com>
2015-12-21 22:25:38 +0100

    Playing with Angular2 Beta

--- app/app.component.js
+++ app/app.component.js
@@ -3,19 +3,23 @@
         .Component({
             selector: 'my-app',
             template: `
-            <h2>Change your Name</h2>
-            <input #cmn>
-            <button (click)="changeMyName(cmn.value)">Change Your Name</button>
-            <h2>My name is {{ myName }}</h2>
-            <p>Names list: </p>
-            <ul>
-                <li *ngFor="#name of names">
-                {{ name }}
-                </li>
-            </ul>
-            <h2>Add more names: </h2>
-            <input #addName>
-            <button (click)="addNames(addName.value)">Add Name</button>
+            <!-- Your Name -->
+                <h2>Change your Name</h2>
+                <input #cmn>
+                <button (click)="changeMyName(cmn.value)">Change Your Name</button>
+                <h2>My name is {{ myName }}</h2>
+            <!-- End Your Name -->
+            <!-- Names List -->
+                <p>Names list: </p>
+                <ul>
+                    <li *ngFor="#name of names">
+                    <p (click)="changeMyName(name)">{{ name }}</p>
+                    </li>
+                </ul>
+                <h2>Add more names: </h2>
+                <input #addName>
+                <button (click)="addNames(addName.value)">Add Name</button>
+            <!-- End Names List -->
             `
         })
         .Class({
