36c88dbe8969879f6b1e878889206b2b9d6fe343
AlbertoFuente <alberto.delafuente.adesis@gmail.com>
2015-12-22 13:20:38 +0100

    Inserted FormComponent bootstrap

--- app/boot.js
+++ app/boot.js
@@ -1,5 +1,6 @@
 (function(app) {
     document.addEventListener('DOMContentLoaded', function() {
+        ng.platform.browser.bootstrap(app.FormComponent);
         ng.platform.browser.bootstrap(app.AppComponent);
     });
 })(window.app || (window.app = {}));
