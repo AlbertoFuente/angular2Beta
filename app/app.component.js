4e0b5d30c59ffb464da557752ce7f39bf2e21d9d
AlbertoFuente <alberto.delafuente.adesis@gmail.com>
2015-12-23 09:56:49 +0100

    Improved app.component.js

--- app/app.component.js
+++ app/app.component.js
@@ -1,37 +1,12 @@
 (function(app) {
+    'use strict';
     app.AppComponent = ng.core
         .Component({
             selector: 'my-app',
-            template: `
-            <!-- Your Name -->
-                <h2>Change your Name</h2>
-                <input #cmn>
-                <button (click)="changeMyName(cmn.value)">Change Your Name</button>
-                <h2>My name is {{ myName }}</h2>
-            <!-- End Your Name -->
-            <!-- Names List -->
-                <p>Names list: </p>
-                <ul>
-                    <li *ngFor="#name of names">
-                    <p (click)="changeMyName(name)">{{ name }}</p>
-                    </li>
-                </ul>
-                <h2>Add more names: </h2>
-                <input #addName>
-                <button (click)="addNames(addName.value)">Add Name</button>
-            <!-- End Names List -->
-            `
+            template: '<form-app></form-app>',
+            directives: [app.FormComponent]
         })
         .Class({
-            constructor: function() {
-                this.myName = 'Alberto';
-                this.names = ['Pedro', 'Juan', 'María', 'Mario', 'Carlos'];
-            },
-            addNames: function(n) {
-                this.names.push(n);
-            },
-            changeMyName: function(n) {
-                this.myName = n;
-            }
+            constructor: function() {}
         });
 }(window.app || (window.app = {})));
