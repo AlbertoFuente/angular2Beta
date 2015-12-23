9f4cbc6f416437fbc3e3db6fb047724bf8200c26
AlbertoFuente <alberto.delafuente.adesis@gmail.com>
2015-12-23 09:55:20 +0100

    Removed ng.platform.browser.bootstrap(app.FormComponent);

--- app/boot.js
+++ app/boot.js
@@ -1,6 +1,5 @@
 (function(app) {
     document.addEventListener('DOMContentLoaded', function() {
-        ng.platform.browser.bootstrap(app.FormComponent);
         ng.platform.browser.bootstrap(app.AppComponent);
     });
 })(window.app || (window.app = {}));
