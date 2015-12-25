46efccc1eb63e2b503833228dd064325627faf5f
AlbertoFuente <alberto.delafuente.adesis@gmail.com>
2015-12-25 22:58:31 +0100

    Added IIFE with app.AppComponent2 Component

--- app/app.component.js
+++ app/app.component.js
@@ -1,10 +1,25 @@
 (function(app) {
     'use strict';
+
     app.AppComponent = ng.core
         .Component({
             selector: 'my-app',
             template: '<form-app></form-app>',
             directives: [app.FormComponent]
+        })
+        .Class({
+            constructor: function() {}
+        });
+}(window.app || (window.app = {})));
+
+(function() {
+    'use strict';
+
+    app.AppComponent2 = ng.core
+        .Component({
+            selector: 'my-app2',
+            template: '<names-list></names-list>',
+            directives: [app.NamesListComponent]
         })
         .Class({
             constructor: function() {}
