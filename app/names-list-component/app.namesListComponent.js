29c837ee6a321c2b7bef6901f839fe860544abc6
AlbertoFuente <alberto.delafuente.adesis@gmail.com>
2015-12-25 22:59:50 +0100

    app.NamesListComponent component

---
+++ app/names-list-component/app.namesListComponent.js
@@ -0,0 +1,22 @@
+(function(app) {
+    'use strict';
+
+    app.NamesListComponent = ng.core
+        .Component({
+            selector: 'names-list',
+            templateUrl: 'app/names-list-component/namesListComponent.html'
+        })
+        .Class({
+            constructor: function() {
+                // name list title
+                this.namesListTitle = 'Names List';
+                // names list
+                this.names = ['Laura', 'Pedro', 'Juan', 'Sandra', 'Pablo'];
+                // button text
+                this.addNameButton = 'Add Name';
+            },
+            insertName: function(name) {
+                this.names.push(name);
+            }
+        });
+}(window.app || (window.app = {})));
