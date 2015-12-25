2487f90a5105b4739bad64a3ca9529e40b32ce14
AlbertoFuente <alberto.delafuente.adesis@gmail.com>
2015-12-25 22:57:57 +0100

    Added app.AppComponent2 to Bootstrap

--- app/boot.js
+++ app/boot.js
@@ -1,5 +1,6 @@
 (function(app) {
     document.addEventListener('DOMContentLoaded', function() {
         ng.platform.browser.bootstrap(app.AppComponent);
+        ng.platform.browser.bootstrap(app.AppComponent2);
     });
 })(window.app || (window.app = {}));
