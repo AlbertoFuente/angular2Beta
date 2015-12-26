a6b6f5d2f8ea4ed645022661c29556f015334e27
AlbertoFuente <alberto.delafuente.adesis@gmail.com>
2015-12-27 00:37:13 +0100

    Added IIFE for app.AppComponent3

--- app/app.component.js
+++ app/app.component.js
@@ -1,3 +1,17 @@
+(function() {
+    'use strict';
+
+    app.AppComponent3 = ng.core
+        .Component({
+            selector: 'tabs-app',
+            template: '<tabs></tabs>',
+            directives: [app.TabsComponent]
+        })
+        .Class({
+            constructor: function() {}
+        });
+}(window.app || (window.app = {})));
+
 (function(app) {
     'use strict';
 
