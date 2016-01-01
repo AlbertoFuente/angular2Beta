628405fbdd25da7a954a6808829666f0698478c1
AlbertoFuente <alberto.delafuente.adesis@gmail.com>
2016-01-01 13:06:57 +0100

    Added method enterKey

--- app/names-list-component/app.namesListComponent.js
+++ app/names-list-component/app.namesListComponent.js
@@ -19,10 +19,16 @@
                 return this.names.findIndex(x => x.toLowerCase() === name.toLowerCase());
             },
             insertName: function(name) {
-                return (this.checkName(name) < 0) ? this.names[this.names.length] = name : false;
+                return (this.checkName(name) < 0 && name !== '') ? this.names[this.names.length] = name : false;
             },
             removeElement: function(name) {
                 this.names.shift(name);
+            },
+            enterKey: function($event) {
+                if ($event.which === 13) {
+                    this.insertName($event.target.value);
+                    $event.target.value = null;
+                }
             }
         });
 }(window.app || (window.app = {})));
